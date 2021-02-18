9d7696d2ba7a479bdf5e25a1972e890c1326a24f
Bicdan <Bicdan22@yandex.ru>
2021-02-19 02:37:55 +0300

    AD 1.0 (wo max freq)

--- cpp/AD.cpp
+++ cpp/AD.cpp
@@ -1,59 +1,90 @@
 #include <iostream>
 #include <ctime>
+#include <vector>
+#include <unistd.h>
 #include "binmatrix.h"
 using namespace std;
 
-struct set{
-    int len;
-    int *elems;
-    set(){
-        len = 0;
-        elems = NULL;
-    }
-    set& operator+=(const int right){
-        for(int k = 0; k < len; k++){
-            if(elems[k] == right) return *this;
-        }
-        elems = (int*) realloc(elems, sizeof(int));
-        elems[len] = right;
-        len++;
-        return *this;
-    }
-    int operator[](int i){
-        assert(len > i);
-        return elems[i];
-    }
-    int getLast(){
-        if(len == 0) return -1;
-        return elems[len-1];
-    }
-    void print(){
-        for(int i = 0; i < len; i++){
-            cout << elems[i] << ' ';
+int main(int argc, char* argv[]){
+    srand(time(NULL));
+    int s = 3;
+    Matrix test = Matrix(6, 6);
+    test.fill();
+    test.print();
+    cout << "***" << endl;
+    Matrix tmp = Matrix(0, 0);
+    Matrix p = Matrix(0, 0);
+    tmp = test;
+    int t = tmp.getFirstComCol(s); // t != -1 (!!!)
+    int l = tmp.getLastComCol(s);
+    vector<int> L(1);
+    L[0] = l;
+    vector<int> Q(0);
+    vector<vector<int>> max_set(0);
+    while(t != -1){
+        Q.push_back(t);
+        tmp.update(t);
+        t = tmp.getFirstComCol(s);
+        for(int i = 0; i < Q.size(); i++){
+            cout << Q[i] << ' ';
         }
         cout << endl;
     }
-    ~set(){
-        if(elems != NULL) delete[] elems;
+    while(Q != L){
+        tmp = test;
+        tmp.update(Q);
+        t = tmp.getFirstComCol(Q[Q.size()-1], s);
+        if(t != -1){
+            Q.push_back(t);
+        }else{
+            max_set.push_back(Q);
+            if(Q.size() == 1){
+                t = test.getFirstComCol(Q[0], s);
+                if(t == -1) break;
+                Q[0] = t;
+            }else if(Q.size() > 1){
+                int pb = Q[Q.size()-1];
+                Q.pop_back();
+                p = test;
+                p.update(Q);
+                int lcom = p.getFirstComCol(pb, s);
+                Q.push_back(pb);
+                if(lcom != -1){
+                    Q.pop_back();
+                    Q.push_back(lcom);
+                }else{
+                    if(Q.size() == 2){
+                        //cout << "b" << endl;
+                        //t = test.getFirstComCol(pb, s);
+                        //if(t == -1) break;
+                        t = test.getFirstComCol(Q[0], s);
+                        Q.clear();
+                        Q.push_back(t);
+                    }else{
+                        //cout << "c" << endl;
+                        int pb = Q[Q.size()-2];
+                        int lcom = Q[Q.size()-1];
+                        Q.pop_back();
+                        Q.pop_back();
+                        //p = test;
+                        //p.update(Q);
+                        //int lcom = p.getFirstComCol(pb, s);
+                        Q.push_back(lcom);
+                    }
+                }
+            }
+        }
+        for(int i = 0; i < Q.size(); i++){
+            cout << Q[i] << ' ';
+        }
+        cout << endl;
     }
-};
-
-int main(int argc, char* argv[]){
-    srand(time(NULL));
-    int s = 100;
-    Matrix test = Matrix(1000, 35);
-    test.fill();
-    Matrix tmp = Matrix(0, 0);
-    tmp = test;
-    int t = tmp.getFirstComCol(s);
-    //set Q = set();
-    while(t != -1){
-        //Q += t;
-        cout << t << ' ';
-        tmp.update(t);
-        t = tmp.getFirstComCol(s);
-    }
-    //Q.print();
-    cout << endl;
+    cout << "***" << endl;
+    /*for(int i = 0; i < max_set.size(); i++){
+        for(int j = 0; j < max_set[0].size(); j++){
+            cout << max_set[i][j] << ' ';
+        }
+        cout << endl;
+    }*/
     return 0;
 }
