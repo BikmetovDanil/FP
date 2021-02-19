623a0e56e234f3bab2af476353598f240b9a7c7d
Bicdan <Bicdan22@yandex.ru>
2021-02-19 03:05:58 +0300

    day 3

--- cpp/AD.cpp
+++ cpp/AD.cpp
@@ -7,28 +7,35 @@ using namespace std;
 
 int main(int argc, char* argv[]){
     srand(time(NULL));
-    int s = 3;
-    Matrix test = Matrix(6, 6);
+    int s = 100;
+    Matrix test = Matrix(1000, 35);
     test.fill();
-    test.print();
-    cout << "***" << endl;
+    //test.print();
+    //cout << "***" << endl;
     Matrix tmp = Matrix(0, 0);
     Matrix p = Matrix(0, 0);
     tmp = test;
-    int t = tmp.getFirstComCol(s); // t != -1 (!!!)
+    int t = tmp.getFirstComCol(s);
+    if(t == -1){
+        cout << "Нет s-совместимых столбцов!" << endl;
+        return 1;
+    }
     int l = tmp.getLastComCol(s);
     vector<int> L(1);
     L[0] = l;
     vector<int> Q(0);
-    vector<vector<int>> max_set(0);
+    //vector<vector<int>> max_set(0);
+    int count = 0;
+    int start = clock();
     while(t != -1){
         Q.push_back(t);
         tmp.update(t);
         t = tmp.getFirstComCol(s);
-        for(int i = 0; i < Q.size(); i++){
+        /*for(int i = 0; i < Q.size(); i++){
             cout << Q[i] << ' ';
         }
-        cout << endl;
+        cout << endl;*/
+        count++;
     }
     while(Q != L){
         tmp = test;
@@ -37,7 +44,7 @@ int main(int argc, char* argv[]){
         if(t != -1){
             Q.push_back(t);
         }else{
-            max_set.push_back(Q);
+            //max_set.push_back(Q);
             if(Q.size() == 1){
                 t = test.getFirstComCol(Q[0], s);
                 if(t == -1) break;
@@ -54,37 +61,28 @@ int main(int argc, char* argv[]){
                     Q.push_back(lcom);
                 }else{
                     if(Q.size() == 2){
-                        //cout << "b" << endl;
-                        //t = test.getFirstComCol(pb, s);
-                        //if(t == -1) break;
                         t = test.getFirstComCol(Q[0], s);
                         Q.clear();
                         Q.push_back(t);
                     }else{
-                        //cout << "c" << endl;
                         int pb = Q[Q.size()-2];
                         int lcom = Q[Q.size()-1];
                         Q.pop_back();
                         Q.pop_back();
-                        //p = test;
-                        //p.update(Q);
-                        //int lcom = p.getFirstComCol(pb, s);
                         Q.push_back(lcom);
                     }
                 }
             }
         }
-        for(int i = 0; i < Q.size(); i++){
+        /*for(int i = 0; i < Q.size(); i++){
             cout << Q[i] << ' ';
         }
-        cout << endl;
+        cout << endl;*/
+        count++;
     }
-    cout << "***" << endl;
-    /*for(int i = 0; i < max_set.size(); i++){
-        for(int j = 0; j < max_set[0].size(); j++){
-            cout << max_set[i][j] << ' ';
-        }
-        cout << endl;
-    }*/
+    //cout << "***" << endl;
+    int end = clock();
+    cout << "Кол-во наборов: " << count << endl;
+    cout << "Время в мс: " << (end - start) * 1000 / CLOCKS_PER_SEC << endl;
     return 0;
 }
